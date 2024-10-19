139c1647c73c74dca8c4a73f7795782fce80660b
Salvatore Rivieccio <draugvar@Macbookvar.station>
2024-10-19 13:46:25 +0200

    first commit

--- examples/PartyThreadsExample.cpp
+++ examples/PartyThreadsExample.cpp
@@ -1,21 +1,49 @@
+/*
+*  PartyThreadsExample.cpp
+ *  PartyThreads
+ *
+ *  Copyright (c) 2019 Salvatore Rivieccio. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #include <iostream>
 #include "PartyThreads.h"
 
 PartyThreads::Pool pt;
 
-void init()
+void init(std::promise<void>& aPromise)
 {
     auto lambda = [] (const int i)
     {
         std::cout << "Hello, " << i << std::endl;
     };
     pt.push(lambda, 1);
+    aPromise.set_value();
 }
 
 int main()
 {
-    auto future = std::async(std::launch::async, init);
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    std::promise<void> aPromise;
+    auto async = std::async(std::launch::async, init, std::ref(aPromise));
+    aPromise.get_future().get();
+    async.get();
     pt.stop();
     return 0;
 }
